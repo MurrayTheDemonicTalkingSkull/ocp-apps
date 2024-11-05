9e65a42e9390f704667a7df6dcb2b7cd1c1e0486
Manos Aggelou <manos05@gmail.com>
2024-11-05 13:17:50 +0200

    first commit

---
+++ simple-web-server/web_server.py
@@ -0,0 +1,20 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+
+class HelloWorldHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        # Respond with "200 OK" and specify HTML content type
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        # Write the response body
+        self.wfile.write(b"Hello, World!")
+
+
+# Define the server address and port
+server_address = ('', 8000)
+httpd = HTTPServer(server_address, HelloWorldHandler)
+
+print("Server running on port 8000...")
+httpd.serve_forever()
